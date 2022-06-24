49d9db95b2ca0ba7befa43cc3120d2f805ffbe4e
kyosukemine <k3.mine.0131@gmail.com>
2022-06-24 19:34:56 +0900

    🎉

--- realtimeplot.py
+++ realtimeplot.py
@@ -2,9 +2,14 @@ import numpy as np
 
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore
+from time import time
+
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
+
 
 app = pg.mkQApp("Plotting Example")
-#mw = QtWidgets.QMainWindow()
+# mw = QtWidgets.QMainWindow()
 # mw.resize(800,800)
 
 win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
@@ -15,21 +20,27 @@ win.setWindowTitle('pyqtgraph example: Plotting')
 pg.setConfigOptions(antialias=True)
 
 p6 = win.addPlot(title="Updating plot")
-curve = p6.plot(pen='y')
+curve = p6.plot(pen='b')
 data = np.random.normal(size=(10, 1000))
 ptr = 0
 data = np.zeros([100])
 data = np.sin
-x = np.linspace(0, 2*np.pi, 500)
+x = np.linspace(0, 5*np.pi, 5000)
 y = np.sin(x)
+old = time()
+now = time()
 
 
 def update():
-    global curve, data, ptr, p6
-    curve.setData(np.sin(x+ptr))
+    global curve, data, ptr, p6, now, old
+    now = time()
+    curve.setData(np.sin(x+ptr*0.1))
     if ptr == 0:
         p6.enableAutoRange('xy', False)  # stop auto-scaling after the first data set is plotted
     ptr += 1
+    if ptr % 10 == 0:
+        p6.setTitle((now-old)*1000)
+    old = now
 
 
 timer = QtCore.QTimer()
